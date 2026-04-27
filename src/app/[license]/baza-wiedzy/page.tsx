9ded917f1cd21e99e9258c8ac1b7d36658714dbc
Filip Krawczyk <kotmawszy@gmail.com>
2026-04-27 14:20:50 +0200

    Update metadata

--- src/app/[license]/baza-wiedzy/page.tsx
+++ src/app/[license]/baza-wiedzy/page.tsx
@@ -1,4 +1,10 @@
+import { metadataBuilder } from "~/app/seo";
 import { db } from "~/server/db";
+
+export const generateMetadata = metadataBuilder((url, name) => ({
+  title: `Baza wiedzy - ${name.short}`,
+  description: `Przeglądaj materiały edukacyjne, notatki i podsumowania do nauki przed egzaminem na licencję ${name.short}.`,
+}));
 
 export default function Page() {
   return null;
