7397f1d4e876881cdf0f1021979502656c1066a0
Filip Krawczyk <kotmawszy@gmail.com>
2026-05-09 09:53:40 +0200

    Make knowledge base statically rendered

--- src/app/[license]/baza-wiedzy/[kbn_id]/page.tsx
+++ src/app/[license]/baza-wiedzy/[kbn_id]/page.tsx
@@ -1,13 +1,41 @@
 import { KnowledgeBaseExplanations } from "~/app/_components/knowledge-base/kb-explanations";
-
-export const dynamic = "force-static";
+import { db } from "~/server/db";
+import { licenses } from "~/server/db/license";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
+import { knowledgeBaseNodes } from "~/server/db/knowledgeBase";
+import { getExplanationsForKnowledgeBaseNode } from "~/server/api/routers/explanation";
 
 export default async function KnowledgeBaseNodePage({
   params,
 }: {
   params: Promise<{ kbn_id: string; license: string }>;
 }) {
-  const { kbn_id } = await params;
+  const { kbn_id, license: licenseUrl } = await params;
+  const license = (
+    await db.select().from(licenses).where(eq(licenses.url, licenseUrl))
+  )[0];
 
-  return <KnowledgeBaseExplanations knowledgeBaseNodeId={kbn_id} />;
+  if (!license) {
+    notFound();
+  }
+
+  const data = await getExplanationsForKnowledgeBaseNode(kbn_id, license.id);
+
+  return <KnowledgeBaseExplanations data={data} knowledgeBaseNodeId={kbn_id} />;
+}
+
+export async function generateStaticParams() {
+  const allKnowledgeBaseNodes = await db
+    .select({ id: knowledgeBaseNodes.id })
+    .from(knowledgeBaseNodes);
+
+  const allLicenses = await db.select({ url: licenses.url }).from(licenses);
+
+  return allKnowledgeBaseNodes.flatMap((node) =>
+    allLicenses.map((license) => ({
+      kbn_id: node.id,
+      license: license.url,
+    })),
+  );
 }
