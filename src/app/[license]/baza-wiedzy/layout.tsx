ae658939e24d19c949785eef5f3ab54f5db48e43
Filip Krawczyk <kotmawszy@gmail.com>
2026-05-17 16:56:15 +0200

    Cache knowledge base tree

--- src/app/[license]/baza-wiedzy/layout.tsx
+++ src/app/[license]/baza-wiedzy/layout.tsx
@@ -1,23 +1,29 @@
 import { asc } from "drizzle-orm";
+import { unstable_cache } from "next/cache";
 import { FolderNode } from "~/app/_components/knowledge-base/tree-node";
 import Main from "~/app/_components/main";
 import type { KnowledgeBaseNode } from "~/server/api/routers/explanation";
 import { db } from "~/server/db";
 import { knowledgeBaseNodes } from "~/server/db/knowledgeBase";
 
-async function buildTree(): Promise<Record<string, KnowledgeBaseNode[]>> {
-  const nodes = await db.query.knowledgeBaseNodes.findMany({
-    orderBy: [asc(knowledgeBaseNodes.parentId), asc(knowledgeBaseNodes.order)],
-  });
-  const children = {} as Record<string, KnowledgeBaseNode[]>;
-  for (const node of nodes) {
-    children[node.parentId ?? "root"] = [
-      ...(children[node.parentId ?? "root"] ?? []),
-      node,
-    ];
-  }
-  return children;
-}
+const buildTree = unstable_cache(
+  async (): Promise<Record<string, KnowledgeBaseNode[]>> => {
+    const nodes = await db.query.knowledgeBaseNodes.findMany({
+      orderBy: [
+        asc(knowledgeBaseNodes.parentId),
+        asc(knowledgeBaseNodes.order),
+      ],
+    });
+    const children = {} as Record<string, KnowledgeBaseNode[]>;
+    for (const node of nodes) {
+      children[node.parentId ?? "root"] = [
+        ...(children[node.parentId ?? "root"] ?? []),
+        node,
+      ];
+    }
+    return children;
+  },
+);
 
 export default async function KnowledgeBaseTree({
   children,
