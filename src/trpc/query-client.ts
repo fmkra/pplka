9447550ffa694164773bd52bedcfa3f0ce645dcf
Filip Krawczyk <kotmawszy@gmail.com>
2026-07-02 20:28:11 +0200

    Relax client query refetching

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -10,7 +10,8 @@ export const createQueryClient = () =>
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
+        staleTime: Number.POSITIVE_INFINITY,
+        refetchOnWindowFocus: false,
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
